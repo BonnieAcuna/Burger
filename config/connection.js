92da6e8dbb1d5ce1c81fd7c80af75d58165dbbfd
Bonnie Acuna <bonnieacuna@Bonnies-MacBook-Pro.local>
2019-02-09 20:16:24 -0700

    styling update

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,7 @@ else {
 
 connection.connect(function(err) {
     if (err) {
-        console.error("error conncecting: " + err.stack);
+        console.error("error connecting: " + err.stack);
         return;
     }
     console.log("connected as id " + connection.threadId);
